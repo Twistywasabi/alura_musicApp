11f44859fb4563a30cc30adbfbde613ac35f10fb
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-09-19 11:41:06 -0300

    Definiton of Audio class

---
+++ src/Audio.java
@@ -0,0 +1,18 @@
+public class Audio {
+    private String Title;
+    private int Duration;
+    private int totalPlays;
+    private int likes;
+    private double ranking;
+
+    public void giveLike(){
+        System.out.println("Audio liked");
+    }
+
+    public void playAudio(){
+        System.out.println("Audio played");
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Audio audio1 = new Audio();
+        audio1.playAudio();
+        audio1.giveLike();
+
+    }
+}
