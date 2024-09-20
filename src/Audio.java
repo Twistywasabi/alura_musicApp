2e85dc81881a742c5990c71408cef049d5c28079
TwistyWasabi17_LG <lucas-hideaki@hotmail.com>
2024-09-20 08:14:07 -0300

    Add Music and Podcast

--- src/Audio.java
+++ src/Audio.java
@@ -5,14 +5,63 @@ public class Audio {
     private int likes;
     private double ranking;
 
-    public void giveLike(){
+    public void giveLike() {
+        this.likes += 1;
         System.out.println("Audio liked");
+        System.out.println("Number of likes: " + this.likes);
     }
 
-    public void playAudio(){
+    public void playAudio() {
         System.out.println("Audio played");
     }
 
+    public void showAudioInfo() {
+        System.out.println("Title: " + getTitle());
+        System.out.println("Duration: " + getDuration() + " seconds.");
+        System.out.println("Amount of time played:" + getTotalPlays() + " times.");
+        System.out.println("Number of likes: " + getLikes());
+        System.out.println("Ranking: " + getRanking());
+    }
 
+    ;
 
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getDuration() {
+        return Duration;
+    }
+
+    public void setDuration(int duration) {
+        Duration = duration;
+    }
+
+    public int getTotalPlays() {
+        return totalPlays;
+    }
+
+    public void setTotalPlays(int totalPlays) {
+        this.totalPlays = totalPlays;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public double getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(double ranking) {
+        this.ranking = ranking;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        Audio audio1 = new Audio();
-        audio1.playAudio();
-        audio1.giveLike();
+        Music music1 = new Music();
+        music1.setTitle("This is the day");
+        music1.setDuration(300);
+        music1.setLikes(10);
+        music1.setRanking(5);
+        music1.setTotalPlays(187);
+        music1.showAudioInfo();
+        music1.playAudio();
+        music1.giveLike();
+
+        Podcast podcasts1 = new Podcast();
+        podcasts1.setTitle("Flow Games");
+        podcasts1.setDuration(3600);
+        podcasts1.setLikes(10);
+        podcasts1.setRanking(5);
+        podcasts1.setTotalPlays(187);
+        podcasts1.showAudioInfo();
+        podcasts1.playAudio();
+        podcasts1.giveLike();
 
     }
 }
